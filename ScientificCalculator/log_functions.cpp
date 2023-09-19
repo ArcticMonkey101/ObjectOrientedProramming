564d03b906d678a3c334d618036eebc4cf2814e3
ArcticMonkey101 <vidisha_ponnappa.sias21@krea.ac.in>
2023-09-19 10:38:29 +0530

    Error handling mechanisms added

--- ScientificCalculator/log_functions.cpp
+++ ScientificCalculator/log_functions.cpp
@@ -1,19 +1,44 @@
 #include "log_functions.h"
 #include <cmath>
 #include <stdexcept>
+#include <iostream>
 
 
-double LogFunctions :: ln(double x) {
-    if (x == 0) {
-        throw std::runtime_error("ln of zero is undefined");
+// double LogFunctions :: ln(double x) {
+//     if (x == 0) {
+//         throw std::runtime_error("ln of zero is undefined");
+//     }
+//     return std::log(x);
+// }
+
+// double LogFunctions :: log(double a, double base) {
+//     if (a <= 0 || base <= 0 || base == 1) {
+//         throw std::runtime_error("Invalid logarithm arguments");
+//     }
+//     return std::log(a) / std::log(base);
+// }
+
+double LogFunctions::ln(double x) {
+    try {
+        if (x == 0) {
+            throw std::runtime_error("ln of zero is undefined");
+        }
+        return std::log(x);
+    } catch (const std::runtime_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        throw;
     }
-    return std::log(x);
 }
 
-double LogFunctions :: log(double a, double base) {
-    if (a <= 0 || base <= 0 || base == 1) {
-        throw std::runtime_error("Invalid logarithm arguments");
+double LogFunctions::log(double a, double base) {
+    try {
+        if (a <= 0 || base <= 0 || base == 1) {
+            throw std::runtime_error("Invalid logarithm arguments");
+        }
+        return std::log(a) / std::log(base);
+    } catch (const std::runtime_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        throw;
     }
-    return std::log(a) / std::log(base);
 }
 
--- ScientificCalculator/math_functions.cpp
+++ ScientificCalculator/math_functions.cpp
@@ -1,13 +1,28 @@
 #include "math_functions.h"
+#include <cassert>
 #include <cmath>
+#include <iostream>
 #include <stdexcept>
 
-double MathFunctions :: squareRoot(double x) {
-    if (x < 0) {
-        throw std::runtime_error("Square root of a negative number");
+
+// double MathFunctions :: squareRoot(double x) {
+//     if (x < 0) {
+//         throw std::runtime_error("Square root of a negative number");
+//     }
+//     return std::sqrt(x);
+// }
+
+double squareRoot(double x) {
+    try {
+        if (x < 0) {
+            throw std::runtime_error("Square root of a negative number");
+        }
+        return std::sqrt(x);
+    } catch (const std::runtime_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        throw;
     }
-    return std::sqrt(x);
-}
+    }
 
 double MathFunctions :: power(double base, double exponent) {
     return std::pow(base, exponent);
